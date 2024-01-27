669a717cbe369dfd4d340b0a572cb2bf35389499
Gareth Verhagen <gazzythehag@outlook.com>
2024-01-27 13:15:23 -0700

    Added opening narration to the game

--- scenes/startDate.gd
+++ scenes/startDate.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if Dialogic.current_timeline == null:
-		Dialogic.start('MasonFirstDate')
+		Dialogic.start('OpeningNarration')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
