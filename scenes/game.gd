af8ec85e5bffce245a2673499c69860155a31a7a
Zorka <jacobothegreat@gmail.com>
2024-01-26 22:35:42 -0700

    Made it so that you can actually exit the game

---
+++ scenes/game.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("exit_game"):
+		get_tree().quit()
